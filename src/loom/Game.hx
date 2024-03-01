d59414ff8cfec66e195a2d05e3dd30361fde42c4
frecklebars <popaalex440@gmail.com>
2024-03-01 23:36:52 +0000

    little

--- src/loom/Game.hx
+++ src/loom/Game.hx
@@ -1,2 +1,24 @@
 package loom;
 
+class Game extends hxd.App {
+
+    public var resolutionW: Int;
+    public var resolutionH: Int;
+
+    private var scaleMode: h2d.Scene.ScaleMode;
+
+    public function new(){
+        super();
+    }
+
+    override function init(){
+        resolutionW = 320; // TODO pass ass parameters
+        resolutionH = 200;
+
+        // pixel-perfect scaling
+        scaleMode = LetterBox(resolutionW, resolutionH, true, Center, Center);
+    }
+
+    override function update(dt: Float){
+    }
+}
---
+++ src/loom/Room.hx
@@ -0,0 +1,12 @@
+package loom;
+
+class Room extends h2d.Scene {
+    
+    public function new(){
+        super();
+    }
+
+    public function init(){}
+    public function update(dt: Float){};
+
+}
